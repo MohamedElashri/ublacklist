d7dd98a2ba34117a8ddda78458b8128980b7bfe5
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2022-04-11 03:49:52 +0000

    Update ublacklist.sh

--- shell/ublacklist.sh
+++ shell/ublacklist.sh
@@ -12,6 +12,8 @@ curl -o i6.txt https://raw.githubusercontent.com/rjaus/ublacklist-yelp/main/ubla
 curl -o i7.txt https://raw.githubusercontent.com/h-matsuo/uBlacklist-subscription-for-developer/master/list.txt
 curl -o i8.txt https://raw.githubusercontent.com/wdmpa/content-farm-list/main/uBlacklist.txt
 curl -o i9.txt https://raw.githubusercontent.com/wdmpa/content-farm-list/main/uBlacklist/spam.txt
+curl -o i10.txt https://raw.githubusercontent.com/PRiMENON/uBlacklist/master/list.txt
+
 # Start Merge and Duplicate Removal
 cat i*.txt > merge.txt
 cat merge.txt | grep -v '#' | grep -v '##' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' > temp.txt
