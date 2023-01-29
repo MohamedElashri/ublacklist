af71babdcc6dcdeee0df44fbd36a51355ac4dd0f
Mohamed Elashri <muhammadelashri@gmail.com>
2023-01-29 18:40:35 +0000

    Update ublacklist.sh

--- shell/ublacklist.sh
+++ shell/ublacklist.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 cd shell/src
 
-# Start Download
-curl -o i0.txt https://raw.githubusercontent.com/MohamedElashri/ublacklist/main/list/custom.txt
-curl -o i1.txt https://raw.githubusercontent.com/jhchabran/code-search-blacklist/main/uBlacklist.txt
-curl -o i2.txt https://raw.githubusercontent.com/sanketpatrikar/hn-search-blacklist/main/blacklist.txt
-curl -o i3.txt https://raw.githubusercontent.com/rjaus/ublacklist-pinterest/main/ublacklist-pinterest.txt
-curl -o i4.txt https://raw.githubusercontent.com/arosh/ublacklist-github-translation/master/uBlacklist.txt
-curl -o i5.txt https://raw.githubusercontent.com/arosh/ublacklist-stackoverflow-translation/master/uBlacklist.txt
-curl -o i6.txt https://raw.githubusercontent.com/rjaus/ublacklist-yelp/main/ublacklist-yelp.txt
-curl -o i7.txt https://raw.githubusercontent.com/h-matsuo/uBlacklist-subscription-for-developer/master/list.txt
-curl -o i8.txt https://raw.githubusercontent.com/wdmpa/content-farm-list/main/uBlacklist.txt
-curl -o i9.txt https://raw.githubusercontent.com/wdmpa/content-farm-list/main/uBlacklist/spam.txt
-curl -o i10.txt https://raw.githubusercontent.com/PRiMENON/uBlacklist/master/list.txt
-curl -o i11.txt https://raw.githubusercontent.com/mtgq/ublacklist-subscription/main/uBlacklist.txt
-curl -o i12.txt https://raw.githubusercontent.com/DivineEngine/uBlacklist/master/blacklist.txt
-curl -o i13.txt https://raw.githubusercontent.com/arosh/ublacklist-stackoverflow-translation/master/uBlacklist.txt
-curl -o i14.txt https://raw.githubusercontent.com/NotJustAHippo/ublacklist/main/sitestoblock.txt
-curl -o i15.txt https://raw.githubusercontent.com/nonPointer/uBlacklist-Subscription/master/blacklist.txt
-curl -o i16.txt https://raw.githubusercontent.com/lbydev/uBlacklist/master/blacklist.txt
-curl -o i17.txt https://raw.githubusercontent.com/DejavuMoe/dejavu-personal-black-lists/master/spam-junk.list
-curl -o i18.txt https://raw.githubusercontent.com/DejavuMoe/dejavu-personal-black-lists/master/spam-deny.list
-curl -o i19.txt https://raw.githubusercontent.com/nsde/spam-sites/main/spam-sites.txt
-curl -o i20.txt 
 
+# Loop over links.txt file which contain all the links to curl instead of hardcoding them
+ while read line; do
+    curl -o i$counter.txt $line
+    counter=$((counter+1))
+    echo $counter
+    echo $line
+    echo " "
+  done < sources.txt
+ echo "Done"
+ 
 # Start Merge and Duplicate Removal
-cat i*.txt > merge.txt
-cat merge.txt | grep -v '#' | grep -v '##' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' > temp.txt
+cat i*.txt | sort | uniq > temp.txt
+mv temp.txt i1.txt
+cat i1.txt | grep -v '#' | grep -v '##' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' > temp.txt
 sort temp.txt | uniq > tmp.txt
 sort -n tmp.txt > tmmp.txt
 
